1c8dd98f9dcd5f5ec75658425fe2ee93f5e52803
wannamit <wannamit@gmail.com>
2014-11-24 14:57:08 +0545

    Update and rename get_package_used.sh to library_used.sh

--- get_package_used.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install python-bs4 -y -qq
-sudo apt-get install python-numpy -y -qq
-sudo apt-get install python-matplotlib -y -qq
---
+++ library_used.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install python-bs4 python-numpy python-matplotlib -y -qq
